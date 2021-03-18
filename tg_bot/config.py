d2862ffca0c557849f496a8a9bbe1f6c50ed24dc
illusion999 <79356567+illusion999@users.noreply.github.com>
2021-03-18 12:17:46 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -8,6 +8,6 @@ class Development(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://ewawttkr:JbHw9yQf3Uj5qNxGC2wLvGVY0Z_L1f9Q@satao.db.elephantsql.com:5432/ewawttkr'  # sample db credentials
     MESSAGE_DUMP = '-1001309372845' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
-    SUDO_USERS = [1047091391, 1678331806]  # List of id's for users which have sudo access to the bot.
+    SUDO_USERS = [1047091391]  # List of id's for users which have sudo access to the bot.
     LOAD = []
     NO_LOAD = ['translation']
