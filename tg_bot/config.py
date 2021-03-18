d89827fe3d6e618380e1da7d9373cce08e57439d
illusion999 <79356567+illusion999@users.noreply.github.com>
2021-03-18 12:04:46 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -2,41 +2,12 @@ from tg_bot.sample_config import Config
 
 
 class Development(Config):
-    LOGGER = True
-
-    # REQUIRED
-    API_KEY = "1745756051:AAHs2614ly6ruWYZUUtHBznX-gRbZ9u3mwY"
-    OWNER_ID = 1678331806   # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "@VegetaxD"
-
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'postgres://ewawttkr:JbHw9yQf3Uj5qNxGC2wLvGVY0Z_L1f9Q@satao.db.elephantsql.com:5432/ewawttkr'  # needed for any database modules
-    MESSAGE_DUMP = -1001309372845  # needed to make sure 'save from' messages persist
+    OWNER_ID = 1410616929  # my telegram ID
+    OWNER_USERNAME = "MysticOp"  # my telegram username
+    API_KEY = "1745756051:AAHs2614ly6ruWYZUUtHBznX-gRbZ9u3mwY"  # my api key, as provided by the botfather
+    SQLALCHEMY_DATABASE_URI = 'postgres://ewawttkr:JbHw9yQf3Uj5qNxGC2wLvGVY0Z_L1f9Q@satao.db.elephantsql.com:5432/ewawttkr'  # sample db credentials
+    MESSAGE_DUMP = '-1001309372845' # some group chat that your bot is a member of
     USE_MESSAGE_DUMP = True
+    SUDO_USERS = [1047091391, 1678331806]  # List of id's for users which have sudo access to the bot.
     LOAD = []
-    # sed has been disabled after the discovery that certain long-running sed commands maxed out cpu usage
-    # and killed the bot. Be careful re-enabling it!
-    NO_LOAD = ['translation', 'rss', 'sed']
-    WEBHOOK = False
-    URL = None
-
-    # OPTIONAL
-    SUDO_USERS = [1410616929]  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = [1047091391]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = [970558707]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    DONATION_LINK = 'https://paypal.me/madarchodmanik'  # EG, paypal
-    CERT_PATH = None
-    PORT = 5000
-    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = True
-    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
-    ALLOW_EXCL = False  # Allow ! commands as well as /
-
-
-class Production(Config):
-    LOGGER = False
-
-
-class Development(Config):
-    LOGGER = True
+    NO_LOAD = ['translation']
